5285ca41e4b47b72eaebbfbd2b898fb0f2a7cd13
amirali <amiraliyavari785@gmial.com>
2022-02-20 04:14:34 -0800

    fix the bug

--- components/Main/Second.vue
+++ components/Main/Second.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full sm:w-1/2 h-[92vh] relative z-10" ref="el">
+  <div class="w-full sm:w-1/2 h-[92vh] sm:h-[95vh] relative z-10" ref="el">
     <div
     @click="changeToFirst"
       class="absolute mb-1 sm:hidden animate-pulse bottom-[50%] left-4 cursor-pointer text-pink-500 font-bold select-none"
@@ -21,21 +21,21 @@
     </div>
     <img
       src="/MyImage/vv-min-removebg.png"
-      class="object-cover w-full h-[92vh]"
+      class="object-cover w-full h-[95vh]"
       alt="This is Amirali Yavari Image"
     />
   </div>
 </template>
 <script setup>
 //Swipe to
-    const el = ref(null);
-    const { isSwiping, direction } = useSwipe(el, {
-      onSwipeEnd() {
-        if (direction.value == "RIGHT") {
-          auth.$reset('mainPage')
-        }
-      },
-    });
+const el = ref(null);
+const { isSwiping, direction } = useSwipe(el, {
+  onSwipeEnd() {
+    if (direction.value == "RIGHT") {
+      auth.$reset('mainPage')
+    }
+  },
+});
 //pinia
 import { useAuth } from "@/stores/auth"
 const auth = useAuth()
