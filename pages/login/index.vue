66d5c953771a8e7be95b77ca1926ccc369756b4b
amirali <amiraliyavari785@gmial.com>
2022-02-21 05:38:24 -0800

    add tailwind and push to admin route if data is complete

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,30 +1,42 @@
 <template>
     <div class="w-full h-screen flex items-center justify-center p-2">
-    <FormKit form-class="bg-white w-full sm:w-1/3 p-5 flex flex-col gap-3 justify-center z-30 shadow-md" type="form" v-model="formdata" @submit="formSubmit" submit-label="Login">
+    <FormKit form-class="font-iran bg-white w-full sm:w-1/3 p-5 flex flex-col gap-3 justify-center z-30 shadow-md rounded-sm" type="form" :actions="false" v-model="formdata" @submit="formSubmit" submit-label="Login">
         <FormKit
         placeholder="Please enter the email"
-        input-class="bg-gray-200 p-1 shadow-sm rounded-sm sm:w-2/3"
+        input-class="bg-gray-200 p-1 shadow-sm rounded-sm w-full"
         label="Email"
-        label-class="text-blue-900 font-bold font-iran"
+        label-class="text-blue-900 font-bold "
         type="email"
         validation-visibility="blur"
         validation="required|*email|length:6"
+        :validation-message="{
+            email:'Email is required'
+        }"
+        message-class="text-pink-600 text-sm"
         >
         </FormKit>
         <FormKit
         placeholder="Please enter the Password"
-        input-class="bg-gray-200 p-1 shadow-sm rounded-sm sm:w-2/3"
+        input-class="bg-gray-200 p-1 shadow-sm rounded-sm w-full"
         label="Password"
-        label-class="text-blue-900 font-iran font-bold"
+        label-class="text-blue-900  font-bold"
         type="password"
         validation-visibility="blur"
         validation="required|*password|length:8"
+        :validation-message="{
+            password:'Password is required'
+        }"
+        message-class="text-pink-600 text-sm"
         >
+        </FormKit>
+        <FormKit type="submit" input-class="bg-blue-900 px-3 py-1 text-white shadow-md">
+            Login
         </FormKit>
     </FormKit>
     </div>
 </template>
 <script setup>
+const router = useRouter();
 //form datas
 const formdata=ref();
 //functions
@@ -33,7 +45,7 @@ function formSubmit() {
     if(formdata.value.email_2 === "amiraliyavari785@gmail.com") {
         //check password
         if(formdata.value.password_3 === "Amir1381") {
-            console.log('success')
+            router.push('/admin')
         }else {
             //password wrong error
             console.error('password wrong')
