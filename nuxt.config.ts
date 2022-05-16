78654adfdefca6bfc0539669515e222d8eeae977
amirali <amiraliyavari785@gmail.com>
2022-05-16 11:52:55 -0700

    config: install formkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  modules: ['@formkit/nuxt'],
   buildModules: [
     '@vueuse/nuxt',
     '@pinia/nuxt',
