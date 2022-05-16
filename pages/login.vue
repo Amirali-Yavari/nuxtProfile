13e66f69f31ecf0143f2b789506b3ccdee2e95c7
amirali <amiraliyavari785@gmail.com>
2022-05-16 11:48:42 -0700

    feat: make login page

---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="h-screen w-full flex items-center justify-center">
+        <div class="max-h-[50vh] bg-white max-w-2xl">
+            
+        </div>
+    </div>
+</template>
